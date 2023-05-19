aac554930aa30f6c565a5081c1a9daec4bbbc0d0
Procentaurus <michalski.44@wp.pl>
2023-05-19 21:05:38 +0200

    Added basic communication with api

--- customUser/views.py
+++ customUser/views.py
@@ -20,6 +20,7 @@ class LoginView(generics.GenericAPIView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data
+
         return Response({
             "token": AuthToken.objects.create(user)[1]
         })
